dad35d5e32fa52db96f2948fa185d27677493a71
nicoladelbianco <nicoladelbianco97@gmail.com>
2024-03-01 18:20:01 +0100

    aggiunta categoria citazioni

--- bin/chuck_norris.dart
+++ bin/chuck_norris.dart
@@ -1,5 +1,8 @@
 import 'package:chuck_norris/chuck_norris.dart' as chuck_norris;
 
-void main(List<String> arguments) {
-  print('Hello world: ${chuck_norris.calculate()}!');
+final category = "dev";
+
+void main(List<String> arguments) async {
+  final data = await chuck_norris.getChuck(category);
+  print('$data');
 }
--- lib/chuck_norris.dart
+++ lib/chuck_norris.dart
@@ -1,3 +1,29 @@
-int calculate() {
-  return 6 * 7;
+import "package:http/http.dart" as http;
+import "dart:convert";
+
+Future<ChuckNorris> getChuck([String? category]) async {
+  final String url;
+  if (category != null) {
+    url = "https://api.chucknorris.io/jokes/random?category=$category";
+  } else {
+    url = "https://api.chucknorris.io/jokes/random";
+  }
+
+  final uri = Uri.parse(url);
+  final data = await http.get(uri);
+  final json = jsonDecode(data.body);
+  return ChuckNorris(
+      fact: json["value"], id: json["id"], createdAt: json["created_at"]);
+}
+
+class ChuckNorris {
+  String fact;
+  String id;
+  String createdAt;
+  ChuckNorris({required this.fact, required this.id, required this.createdAt});
+
+  @override
+  String toString() {
+    return "ID: $id\nCreato il: $createdAt\nFact: $fact ";
+  }
 }
